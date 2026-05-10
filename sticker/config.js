f2a445bae5beca4a3546a9a2ce78ae554d76c2cb
KimTaeHyen <62549012+teahyen@users.noreply.github.com>
2026-05-10 23:50:50 +0900

    Add icon, preset star count, update API URL

--- sticker/config.js
+++ sticker/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbztfPin9OINUnL3_WleN3pAxxn6evxgsW8KUt3ZcYbf6LQPLfr6G3sQ2QZOVc5t/exec'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxWKKg7kSL07dCLSQoN71YH2wMAGeufbgJF5sA6LDuEDM_t-KiyhAmV7JcM2wGw7Cg/exec'
 };
