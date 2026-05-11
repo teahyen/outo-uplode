dea633e5c5dd73e40e2f812dd31b87b753f0d717
KimTaeHyen <62549012+teahyen@users.noreply.github.com>
2026-05-11 09:14:46 +0900

    Update Apps Script URL

--- sticker/config.js
+++ sticker/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyKRIsVmdbqSrmU9wlmD78W6Yp8f_KpSvT5QDRSaC-l3oh7LGxs4g18c44Dni0KrDA/exec'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyLYxZcsG4FWW9B42ATE5m14-xPLxsDLDEqO2ImC_aGEtAn3T-Ia9i_oZ7W9m9XN9c/exec'
 };
